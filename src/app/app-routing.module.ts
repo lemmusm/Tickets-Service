e54fab3f7f8572483a4f08273218963953a52101
Manuel Hernandez Lemus <lemmusm@gmail.com>
2019-01-12 21:47:44 -0600

    Actualizacion del app-routing.module
    	Ruta para el componente dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/pages/login/login.component';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
   },
   {
     path: '**', pathMatch: 'full', redirectTo: 'login' 
