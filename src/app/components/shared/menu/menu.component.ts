4e75664b07e066b5de47432752ce4a43c9772401
Manuel Hernandez Lemus <lemmusm@gmail.com>
2019-01-17 09:31:02 -0600

    Actualizacion de componentes
    	.- Agregar menu al navbar
    	.- Actualizacion de rutas en menu

--- src/app/components/shared/menu/menu.component.ts
+++ src/app/components/shared/menu/menu.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/providers/api.service';
+import { AuthService } from 'src/app/providers/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apiservice: ApiService, private authservice: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
+  // Comrpobar si el usuario no ha actualizado su perfil por primera vez
+  verifyUpdatePerfil() {
+    this.apiservice.getUsuarioByUID(this.authservice.usuario.uid)
+        .subscribe(
+          (response: any) => {
+            if (response.departamento_id === 2) {
+              this.router.navigate(['editarUsuario']);
+            } else {
+              console.log('Estas actualizado');              
+            }
+          },
+          error => {
+            console.log(error);
+          }
+        );
+  }
 }
