9136f5ba0461d4798531f40943f49e9b5dda04c8
Manuel Hernandez Lemus <lemmusm@gmail.com>
2019-01-17 08:58:32 -0600

    Instalacion de dependencias, update  app.module
    	.- Instalacion Angular-Datatable
    	.- Actualizacion app.module
    		- LOCALE_ID español
    		- DataTables
    		- AngularFire
    		- Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+// LOCALE
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEs);
 // angularfire
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
+// HTTP
+import { HttpClientModule } from '@angular/common/http';
+// datatables
+import { DataTablesModule } from 'angular-datatables';
 // componentes
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +19,9 @@ import { DashboardComponent } from './components/pages/dashboard/dashboard.compo
 import { LoginComponent } from './components/pages/login/login.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
+import { ListaUsuarioComponent } from './components/pages/usuario/lista-usuario/lista-usuario.component';
+import { MenuComponent } from './components/shared/menu/menu.component';
+import { EditarUsuarioComponent } from './components/pages/usuario/editar-usuario/editar-usuario.component';
 
 @NgModule({
   declarations: [
@@ -18,15 +29,20 @@ import { FooterComponent } from './components/shared/footer/footer.component';
     DashboardComponent,
     LoginComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    ListaUsuarioComponent,
+    MenuComponent,
+    EditarUsuarioComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    HttpClientModule,
+    DataTablesModule
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'es' } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
