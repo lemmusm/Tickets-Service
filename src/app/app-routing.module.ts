2ddeee7b4489846ca7b012f630509890ec1cc81c
Manuel Hernandez Lemus <lemmusm@gmail.com>
2019-03-13 10:00:21 -0600

    Update 1.2.1
    	.- Preload all modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { LoginComponent } from './components/pages/login/login.component';
 import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
 import { GuardService } from './providers/guard.service';
@@ -105,6 +105,7 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes, {
       useHash: true,
+      preloadingStrategy: PreloadAllModules,
       scrollPositionRestoration: 'enabled'
     })
   ],
